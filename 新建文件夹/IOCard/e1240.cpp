a838d86a8b57db068ff5239f1ad06bd070075f95
LiBang0613 <1107614228@qq.com>
2019-06-05 19:38:17 +0800

    完善逻辑

---
+++ 新建文件夹/IOCard/e1240.cpp
@@ -0,0 +1,96 @@
+﻿#include "e1240.h"
+
+E1240::E1240()
+{
+
+}
+
+E1240::~E1240()
+{
+
+}
+
+bool E1240::Query_Read(QByteArray &sendBuf, int &nLen)
+{
+    int i = 0;
+
+    // 1.
+    for (i = 0; i < 5; i++)
+        sendBuf[i] = 0x00;
+
+    // 2.
+    sendBuf[i++] = 0x06;
+    sendBuf[i++] = 0x01;
+    sendBuf[i++] = 0x04;
+    sendBuf[i++] = 0x00;
+    sendBuf[i++] = 0x00;
+    sendBuf[i++] = 0x00;
+    sendBuf[i++] = m_nBitCount;	// 8个AI
+
+    nLen = i;
+    return true;
+}
+
+bool E1240::Response_Read(QByteArray recvBuf, int nLen)
+{
+    // 1. 接收长度校验
+    if ( nLen != 0x05 + 0x03 + 0x01 + m_nBitCount * 2)
+    {
+        m_nFailedTimes++;
+        return false;
+    }
+    // 2. 功能码+数据长度校验
+    if ( recvBuf[6].operator !=(0x01)
+         || recvBuf[7].operator !=(0x04)
+         || recvBuf[8].operator !=(m_nBitCount * 2)
+         )
+    {
+        m_nFailedTimes++;
+        return false;	// 错误
+    }
+    //	CSafeLock cLock(&m_cMutex);
+
+    ushort wVal = 0;
+
+    for (int i = 0; i < m_nBitCount; i++)
+    {
+        uchar ucB1  = recvBuf[i * 2 + 8 + 1];
+        uchar ucB2  = recvBuf[i * 2 + 8 + 2];
+        wVal = 	(ucB1 << 8) | ucB2;
+    }
+    return true;
+}
+
+void E1240::Process()
+{
+    QThread::sleep(m_nTimeInterval);
+    QByteArray sendArray;
+    int nSendLen = 0;
+    Query_Read(sendArray,nSendLen);
+    m_sendArray = sendArray;
+//    qDebug()<<"1240"<<QThread::currentThreadId()<<m_sendArray<<m_sendArray.size();
+    m_qTcpSocket->write(sendArray);
+    m_nSendTimes++;
+    if(nSendLen != sendArray.size())
+    {
+        qDebug()<<"send error";
+        return;
+    }
+
+    if(m_qTcpSocket->waitForReadyRead() == false)
+    {
+        return ;
+    }
+
+}
+
+void E1240::slt_readyRead()
+{
+    //父类的接口，数采卡返回数据响应的槽函数
+
+    QByteArray recvArray = m_qTcpSocket->readAll();
+    Response_Read(recvArray,recvArray.size());
+//    qDebug()<<"1240"<<QThread::currentThreadId()<<recvArray<<recvArray.size();
+    emit sig_statisticsCounts(m_strIp,m_nSendTimes,m_nFailedTimes);
+    emit sig_sendRecv(m_strIp+" type:1240",m_sendArray,recvArray);
+}
---
+++ 新建文件夹/IOCard/iocard.h
@@ -0,0 +1,96 @@
+﻿#ifndef IOCARD_H
+#define IOCARD_H
+
+#include "iocard_global.h"
+#include <QObject>
+#include <QTcpSocket>
+#include <QThread>
+#include <QTimer>
+
+enum WriteState
+{
+     wsIni = 1
+    ,wsReady = 2
+    ,wsEnd = 3
+};
+struct CtrlDO
+{
+    unsigned short	wID;		// 组ID标识
+    int		nRegAddr;
+    int		nRegCount;
+
+    unsigned short	wCmd;		// 命令值
+    WriteState	wState;		// 写操作-状态
+
+public:
+    CtrlDO()
+    {
+        wID			= 0;		// 组ID标识
+        nRegAddr	= 0;
+        nRegCount	= 0;
+
+        wCmd		= 0;		// 命令值
+        wState		= wsEnd;	// 写操作状态标记
+    }
+};
+class IOCARDSHARED_EXPORT IOCard : public QObject
+{
+    Q_OBJECT
+public:
+    IOCard();
+    virtual ~IOCard();
+public:
+    //连接指定Ip地址
+    bool Open(QString strIp,uint nPort = 502);
+    //设置读写寄存器 /线圈数量
+    virtual void setBitCount(int nCount);
+    //启动通讯线程
+    void startThread();
+    //停止通讯线程
+    void stopThread();
+
+    void setTimeInterval(int second);
+protected:
+    //虚函数，子类实现
+    virtual void Process() = 0;
+
+protected slots:
+    void slt_tcpConnected();
+    void slt_recvSocketState(QAbstractSocket::SocketState state);
+    void slt_tcpDisConnected();
+    virtual void slt_readyRead() = 0;
+    void slt_ProThread();
+    void slt_newSocket();
+    void slt_stopThread();
+
+signals:
+    //统计数据信号，发送多少次命令，失败多少次
+    void sig_statisticsCounts(QString,int,int);
+    //发送内容信号
+    void sig_sendRecv(QString,QByteArray,QByteArray);
+
+    void sig_newSocket();
+
+    void sig_stopThread();
+
+    void sig_operate();
+protected:
+    QThread *m_pThread;
+    QTcpSocket *m_qTcpSocket;
+    QString m_strIp;
+    int m_nBitCount;
+    int m_nSendTimes;
+    int m_nFailedTimes;
+    int m_nTimeInterval;
+    QTimer m_cycleTimer;
+public:
+    QByteArray m_sendArray;
+
+private:
+    bool m_bNewSocket;
+
+    bool m_bExitThread;
+
+};
+
+#endif // IOCARD_H
